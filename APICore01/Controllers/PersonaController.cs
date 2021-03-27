a871c5ad790bb1636cad6a7207d66308db05ff25
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-03-27 10:41:16 -0600

    Initial commit

---
+++ APICore01/Controllers/PersonaController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICore01.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class PersonaController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            //Conexion a db
+            using (Models.CRUDAPIHDContext db = new Models.CRUDAPIHDContext())
+            {
+                //Consulta con LINQ
+                var lst = (from d in db.Personas
+                           select d).ToList();
+
+                return Ok(lst);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Request.PersonaRequest model) {
+            //Conexion a db
+            using (Models.CRUDAPIHDContext db = new Models.CRUDAPIHDContext())
+            {
+                Models.Persona oPersona = new Models.Persona();
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Personas.Add(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+
+        //Modificar un registro
+        [HttpPut]
+        public ActionResult Post([FromBody] Models.Persona model)
+        {
+            //Conexion a db
+            using (Models.CRUDAPIHDContext db = new Models.CRUDAPIHDContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Entry(oPersona).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        //Eliminar un registro
+        [HttpDelete]
+        public ActionResult Put([FromBody] Models.Persona model) // Indica que el id va en el cuerpo
+        {
+            //Conexion a db
+            using (Models.CRUDAPIHDContext db = new Models.CRUDAPIHDContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                db.Personas.Remove(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ APICore01/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICore01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            // Coneccion a la base de datos
+            using (Models.CRUDAPIHDContext db = new Models.CRUDAPIHDContext())
+            {
+                // Hacer una consulta con LINQ
+                var lst = (from d in db.Personas
+                          select d).ToList();
+
+                return Ok(lst);
+            }
+        }
+    }
+}
